21f83015b67d6a0b1e8e67455fa0ff2d0c030406
aelkz <raphael.alex@gmail.com>
2017-02-08 22:35:44 -0200

    Initial commit

---
+++ src/main/java/com/github/aelkz/backend/app/Application.java
@@ -0,0 +1,18 @@
+package com.github.aelkz.backend.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    /*
+     * remeber to bootstrap your mongoDB instance.
+     * https://www.mongodb.com/download-center#community
+     * https://docs.mongodb.com/manual/tutorial/install-mongodb-on-windows/
+     * $ mongod.exe
+     */
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(new Object[] { Application.class }, args);
+    }
+
+}
